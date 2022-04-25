8610f003c0417cb407deff00749b98c1b9bf3f99
jeremy <poohc300@gmail.com>
2022-04-25 17:40:25 +0900

    solve prob3

--- java-study/practice01/src/main/java/prob3/Prob3.java
+++ java-study/practice01/src/main/java/prob3/Prob3.java
@@ -9,7 +9,26 @@ public class Prob3 {
 		Scanner scanner = new Scanner(System.in);
 
 		/* 코드 작성 */
+		System.out.println("숫자를 입력하세요:");
+		int number = scanner.nextInt();
+		int i = 0;
+		int sum = 0;
 		
+		for(i=0;i<number+1;i++) {
+			if(number % 2 == 1) {
+				if(i % 2 == 1) {
+					sum+=i;
+				}
+			}
+			else if(number % 2 == 0) {
+				if(i % 2 == 0) {
+					sum+=i;
+				}
+			}
+			
+			
+		}
+		System.out.println("결과 값: "+sum);
 		scanner.close();
 	}
 }
