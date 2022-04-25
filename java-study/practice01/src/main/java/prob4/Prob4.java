f682889b9eab6adc636ace616a6c9b12669842d0
jeremy <poohc300@gmail.com>
2022-04-25 17:59:13 +0900

    solve prob4

--- java-study/practice01/src/main/java/prob4/Prob4.java
+++ java-study/practice01/src/main/java/prob4/Prob4.java
@@ -9,9 +9,12 @@ public class Prob4 {
 
 		System.out.print("문자열을 입력하세요 : ");
 		String text = scanner.nextLine();
-
-		
 		scanner.close();
+		
+		int i = 0;
+		for(i=0;i<text.length()+1;i++) {
+			System.out.println(text.substring(0,i));
+		}
 	}
 
 }
