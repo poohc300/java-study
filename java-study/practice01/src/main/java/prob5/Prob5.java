24e7beefa8c57406540b8f88b9bb5edcc5d078f8
jeremy <poohc300@gmail.com>
2022-04-26 10:40:30 +0900

    delete unused docs

--- java-study/practice01/src/main/java/prob5/Prob5.java
+++ java-study/practice01/src/main/java/prob5/Prob5.java
@@ -3,5 +3,11 @@ package prob5;
 public class Prob5 {
 
 	public static void main(String[] args) {
+		int i = 0;
+		String result = "";
+		for(i=30; i<33; i++) {
+			result = String.valueOf(i);
+			System.out.println(result.contains("3"));
+		}
 	}
 }
