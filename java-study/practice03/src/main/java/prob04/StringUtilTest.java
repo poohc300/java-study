62ef39bc69b936a1483b77419fc4482c0382c854
jeremy <poohc300@gmail.com>
2022-04-27 18:14:47 +0900

    solve practice03-prob04

---
+++ java-study/practice03/src/main/java/prob04/StringUtilTest.java
@@ -0,0 +1,15 @@
+package prob04;
+
+public class StringUtilTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	String[] strArr = {"SuperMan", "BatMan", "SpiderMan"}; 
+    String resultStr = StringUtil.concatenate( strArr );
+
+    System.out.println( "결과 문자열 : " + resultStr ); 
+
+
+	}
+
+}
